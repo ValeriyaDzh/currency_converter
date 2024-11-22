3c00656d24cf0da505d52362f9e3b4a0d869c4a0
Valeriya <Dzhavadova.v@yandex.ru>
2024-11-22 11:39:27 +0300

    feat: add logger

--- src/main.py
+++ src/main.py
@@ -1,3 +1,20 @@
+import logging
+import logging.config
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI(title="CurrencyToday")
+from src.api.v1 import router_v1
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logging.config.fileConfig("logging.ini", disable_existing_loggers=False)
+    logger = logging.getLogger(__name__)
+    logger.info("Starting app...")
+    yield
+
+
+app = FastAPI(title="CurrencyToday", lifespan=lifespan)
+
+app.include_router(router_v1, prefix="/api")
